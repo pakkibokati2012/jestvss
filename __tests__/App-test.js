eeb536878f2a3cea2071f8a14813237b557a350a
prakash <prakash.bokati@seelogic.co.uk>
2019-04-09 17:12:29 +0545

    finally did it

--- __tests__/App-test.js
+++ __tests__/App-test.js
@@ -11,19 +11,27 @@ it('renders correctly', () => {
   renderer.create(<App />);
 });
 
-jest.mock('NativeModules', () => ({
-  UIManager: {
-    RCTView: () => ({
-      directEventTypes: {}
-    })
-  },
-  KeyboardObserver: {},
-  RNGestureHandlerModule: {
-    attachGestureHandler: jest.fn(),
-    createGestureHandler: jest.fn(),
-    dropGestureHandler: jest.fn(),
-    updateGestureHandler: jest.fn(),
-    State: {},
-    Directions: {}
-  }
-}));
+jest.mock('react-navigation', () => {
+  return {
+    createAppContainer: jest
+      .fn()
+      .mockReturnValue(function NavigationContainer(props) {
+        return null;
+      }),
+    createDrawerNavigator: jest.fn(),
+    createMaterialTopTabNavigator: jest.fn(),
+    createStackNavigator: jest.fn(),
+    createSwitchNavigator: jest.fn(),
+    StackActions: {
+      push: jest
+        .fn()
+        .mockImplementation(x => ({ ...x, type: 'Navigation/PUSH' })),
+      replace: jest
+        .fn()
+        .mockImplementation(x => ({ ...x, type: 'Navigation/REPLACE' }))
+    },
+    NavigationActions: {
+      navigate: jest.fn().mockImplementation(x => x)
+    }
+  };
+});
